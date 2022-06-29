bc6797bd6049c15db830b04ba91a15ffb4590c03
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-06-30 00:30:40 +0700

    react_router_v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-// import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-const  App = ()=> (
-  <div>
-    <p>asd</p>
-  </div>
-)
+//authentication
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+//Pages
+import Home from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
+const  App = ()=> {
+  return (
+    <Routes>
+      <Route path="*" element={<PageNotFound/>}/>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/login" element={<Login/>}/>
+     <Route path="/register" element={<Register/>}/>
+    </Routes>
+  )
+ }
 
 export default App;
