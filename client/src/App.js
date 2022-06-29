5476da639e39ab4351a3f945209feb897562b653
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-06-30 02:08:08 +0700

    import_antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React  from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 //authentication
-import Login from "./pages/auth/Login"
-import Register from "./pages/auth/Register"
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 //Pages
-import Home from './pages/Home'
-import PageNotFound from './pages/PageNotFound'
-const  App = ()=> {
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Header from "./components/nav/Header";
+const App = () => {
   return (
-    <Routes>
-      <Route path="*" element={<PageNotFound/>}/>
-     <Route path="/" element={<Home/>}/>
-     <Route path="/login" element={<Login/>}/>
-     <Route path="/register" element={<Register/>}/>
-    </Routes>
-  )
- }
+    <>
+      <Header />
+      <Routes>
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
