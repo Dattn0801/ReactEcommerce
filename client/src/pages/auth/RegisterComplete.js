53955adf45e65f68e0afa9e4f5b2c7b6e92967fa
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-07-07 23:24:48 +0700

    add_register_complete

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,30 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { sendSignInLinkToEmail } from "firebase/auth";
+import { signInWithEmailLink, updatePassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const RegisterComplete = ({ history }) => {
+  let navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  useState(() => {
-    console.log(localStorage.getItem("emailForSignIn"));
+  useEffect(() => {
+    setEmail(window.localStorage.getItem("emailForSignIn"));
   }, []);
   const handleSumit = async (e) => {
     e.preventDefault();
+    //Validate
+    if (!email || !password) {
+      toast.error("Cần tài khoản email và mật khẩu");
+      return;
+    }
+    if (password.length < 6) {
+      toast.error("Mật khẩu hơn 6 kí tự");
+      return;
+    }
+    try {
+      const result = await signInWithEmailLink(
+        auth,
+        email,
+        window.location.href
+      );
+
+      //   console.log("RESULT", result);
+      if (result.user.emailVerified) {
+        //remove user from local storage
+        window.localStorage.removeItem("emailForSignIn");
+        //get user id token
+        let user = auth.currentUser;
+
+        await updatePassword(user, password);
+        const idTokenResult = await user.getIdTokenResult();
+        //redux store
+        // console.log("user", user, "idtokenrs", idTokenResult);
+        //redirect
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error);
+    }
   };
   const compelteRegistrationForm = () => (
     <form onSubmit={handleSumit}>
+      <input type="email" className="form-control" value={email} disabled />
+      <br />
       <input
-        type="email"
+        type="password"
         className="form-control"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         autoFocus
+        placeholder="Nhập mật khẩu mới"
       />
       <br />
       <button type="submit" className="btn btn-outline-secondary">
-        Hoàn tất đăng nhập
+        Hoàn tất đăng kí
       </button>
     </form>
   );
@@ -32,7 +72,7 @@ const RegisterComplete = ({ history }) => {
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h4>Form đăng kí</h4>
+          <h4>Hoàn tất đăng kí</h4>
           {compelteRegistrationForm()}
         </div>
       </div>
