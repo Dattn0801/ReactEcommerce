81c9107d138202279611b2469cbeeed203e75f58
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-07-22 20:37:16 +0700

    nav_history_link

--- client/src/components/nav/UserNav.js
+++ client/src/components/nav/UserNav.js
@@ -2,19 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const UserNav = () => {
-  <nav>
-    <ul className="nav flex-column">
+  return (
+    <nav>
+      <ul className="nav flex-column">
         <li className="nav-item">
-            <Link to="user/history">History</Link>
+          <Link to="user/history" className="nav-link">
+            History
+          </Link>
         </li>
         <li className="nav-item">
-            <Link to="user/password">Password</Link>
+          <Link to="user/password" className="nav-link">
+            Password
+          </Link>
         </li>
         <li className="nav-item">
-            <Link to="user/wishlist">Wishlist</Link>
+          <Link to="user/wishlist" className="nav-link">
+            Wishlist
+          </Link>
         </li>
-    
-    </ul>
-  </nav>;
+      </ul>
+    </nav>
+  );
 };
 export default UserNav;
