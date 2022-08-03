c445443ba11f9a4e814aebf0dd24f093da3ee8fc
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-08-03 22:25:32 +0700

    product_home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getProductByCount } from "../functions/product";
+import ProductCard from "../components/cards/ProductCard";
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
 
-const Home = () =>{
-    return(
-        <div>
-        <p>Home Page</p>
+  const loadAllProducts = async () => {
+    setLoading(true);
+    getProductByCount(4).then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+    });
+  };
+  return (
+    <>
+      <div className="jumbotron">
+        {loading ? <h4>Loading...</h4> : <h4>Tất cả sản phẩm</h4>}
+      </div>
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div key={product._id} className="col-md-3">
+              <ProductCard product={product} />
+            </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default Home;
