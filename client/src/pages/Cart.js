d791c97390502345fcef90b3c5dcba73358712c8
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-09-08 02:32:49 +0700

    cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,7 +5,12 @@ import { Link } from "react-router-dom";
 const Cart = () => {
   const { cart, user } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
-
+  const getTotal = () => {
+    return cart.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
+  const saveOrderToDb = () => {};
   return (
     <div className="container-fluid">
       <div className="row">{/* <h2>{JSON.stringify(cart)}</h2> */}</div>
@@ -20,19 +25,35 @@ const Cart = () => {
           )}
         </div>
         <div className="col-md-4">
-          <h4>order summary</h4>
+          <h4>Đơn hàng</h4>
           <hr />
           <p>Sản phẩm</p>
-          {cart.map((c, i) => {
+          {cart.map((c, i) => (
             <div key={i}>
-            <p></p>
-            </div>;
-          })}
+              <p>
+                <span className="text-primary"> {c.title}</span> x
+                <span className="text-danger">{c.count} </span>= $
+                {c.price * c.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          Tổng tiền <b>${getTotal()}</b>
+          <hr />
           {user ? (
-            <button className="btn btn-sm btn-primary mt-2"> Thanh toán</button>
+            <button
+              onClick={saveOrderToDb}
+              disabled={!cart.length}
+              className="btn btn-sm btn-primary mt-2"
+            >
+              {" "}
+              Thanh toán
+            </button>
           ) : (
             <button className="btn btn-sm btn-primary mt-2">
-              Đăng nhập để thanh toán
+              <Link to="/login" state={{ from: "cart" }}>
+                Đăng nhập để thanh toán
+              </Link>
             </button>
           )}
         </div>
