77cd57ca330acbe74b74391336f6f722fedfa289
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-09-12 20:47:23 +0700

    user_cart_be

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// middlewares
+const { authCheck } = require("../middlewares/auth");
+// controller
+const { userCart, getuserCart } = require("../controllers/user");
+
+router.post("/user/cart", authCheck, userCart);
+router.get("user/cart", authCheck, getuserCart);
 module.exports = router;
