acc8888505cb077d45ebad5456cd74a5a1036964
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-09-14 22:03:08 +0700

    checkout_done

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getUserCart, emptyUserCart } from "../functions/user";
+import { getUserCart, emptyUserCart, saveUserAddress } from "../functions/user";
 import { toast } from "react-toastify";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Checkout = () => {
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState(0);
-
+  const [address, setAddress] = useState("");
+  const [addressSaved, setAddressSaved] = useState(false);
   const dispatch = useDispatch();
   const { user } = useSelector((state) => ({ ...state }));
 
@@ -35,19 +38,24 @@ const Checkout = () => {
     });
   };
   const saveAddressToDb = () => {
-    //
+    //console.log(address)
+    saveUserAddress(user.token, address).then((res) => {
+      if (res.data.ok) {
+        setAddressSaved(true);
+        toast.success("Địa chỉ đã lưu , đặt hàng nào");
+      }
+    });
   };
   return (
     <div className="row">
       <div className=" col-md-6">
-        <h4>Delivery address</h4>
+        <h4>Địa chỉ</h4>
         <br />
-        <textarea rows="" cols=""></textarea>
+        <ReactQuill theme="snow" value={address} onChange={setAddress} />
         <hr />
-        <button
-          className="btn btn-primary mt-2"
-          onClick={saveAddressToDb}
-        ></button>
+        <button className="btn btn-primary mt-2" onClick={saveAddressToDb}>
+          Lưu địa chỉ
+        </button>
         <h4>Got coupon?</h4>
         <br />
         cup
@@ -71,7 +79,13 @@ const Checkout = () => {
 
         <div className="row">
           <div className="col-md-6">
-            <button className="btn btn-primary"> Đặt hàng</button>
+            <button
+              className="btn btn-primary"
+              disabled={!addressSaved || !products.length}
+            >
+              {" "}
+              Đặt hàng
+            </button>
           </div>
           <div className="col-md-6">
             <button
