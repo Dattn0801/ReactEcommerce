2c1cb99d6fe9494b10f14c49370a7413d1970859
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-09-15 05:30:44 +0700

    coupon

--- client/src/pages/admin/coupon/CreateCoupon.js
+++ client/src/pages/admin/coupon/CreateCoupon.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import {
   getCoupons,
-  removeCoupon,
+  deleteCoupon,
   createCoupon,
 } from "../../../functions/coupon";
 //date picker
@@ -17,9 +17,16 @@ const CreateCouponPage = () => {
   const [expiry, setExpiry] = useState("");
   const [discount, setDiscount] = useState("");
   const [loading, setLoading] = useState("");
-
+  const [coupons, setCoupons] = useState([]);
   const { user } = useSelector((state) => ({ ...state }));
 
+  useEffect(() => {
+    loadAllCoupons();
+  }, []);
+
+  const loadAllCoupons = () => {
+    getCoupons().then((res) => setCoupons(res.data));
+  };
   const handleSumit = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -27,12 +34,25 @@ const CreateCouponPage = () => {
     createCoupon({ name, expiry, discount }, user.token)
       .then((res) => {
         setLoading(false);
+        loadAllCoupons();
         setName("");
         setDiscount("");
         setExpiry("");
         toast.success(`tạo coupon ${res.data.name} thành công`);
       })
       .catch((err) => console.log("create coupon err", err));
+  };
+  const handleRemove = (couponId) => {
+    if (window.confirm("Xóa")) {
+      setLoading(true);
+      deleteCoupon(couponId, user.token)
+        .then((res) => {
+          loadAllCoupons();
+          setLoading(false);
+          toast.error(`${res.data.name} đã xóa`);
+        })
+        .catch((err) => console.log(err));
+    }
   };
   return (
     <div className="container-fluid">
@@ -41,7 +61,11 @@ const CreateCouponPage = () => {
           <AdminNav />
         </div>
         <div className="col-md-10">
-          <h4>Coupon</h4>
+          {loading ? (
+            <h4 className="text-danger">Loading ....</h4>
+          ) : (
+            <h4>Mã giảm giá</h4>
+          )}
           <form onSubmit={handleSumit}>
             <div className="form-group">
               <label className="text-muted">Name</label>
@@ -80,6 +104,34 @@ const CreateCouponPage = () => {
             </div>
             <button className="btn btn-outline-primary">Lưu</button>
           </form>
+
+          <h4>{coupons.length} Mã giảm giá</h4>
+
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th className="col">Tên</th>
+                <th className="col">Ngày hết hạn</th>
+                <th className="col">Discount</th>
+                <th className="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coupons.map((c) => (
+                <tr key={c._id}>
+                  <td>{c.name}</td>
+                  <td>{new Date(c.expiry).toLocaleDateString()}</td>
+                  <td>{c.discount} %</td>
+                  <td>
+                    <DeleteOutlined
+                      className="text-danger text-center pointer"
+                      onClick={() => handleRemove(c._id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
