49a7c21dc72bb55c4026ae3284bccfe412fa22ac
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2022-09-14 21:46:05 +0700

    fix_f5_text_input_filter

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -70,6 +70,7 @@ const Shop = () => {
   useEffect(() => {
     const delayed = setTimeout(() => {
       fetchProducts({ query: text });
+      if (!text) loadAllProducts();
     }, 300);
     return () => clearTimeout(delayed);
   }, [text]);
