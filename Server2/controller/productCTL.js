cf556ca19d4cb4542bbdc8de17a8334f66c0cd40
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-02-14 16:44:30 +0700

    product_sort_filter_pagination

--- Server2/controller/productCTL.js
+++ Server2/controller/productCTL.js
@@ -61,11 +61,48 @@ exports.getaProduct = asyncHandler(async (req, res) => {
 //getAll
 
 exports.getAllProduct = asyncHandler(async (req, res) => {
+  //console.log(req.query);
   try {
-    const allProduct = await Product.find({
-      brand: req.query.brand,
-    });
-    res.json(allProduct);
+    //Filter
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    //Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //pagination
+
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("This page does not exists");
+    }
+    console.log(page, limit, skip);
+
+    const product = await query;
+    res.json(product);
+    //res.json(query);
   } catch (error) {
     throw new Error(error);
   }
