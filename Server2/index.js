a74299cdc7faedffe12d202b8c33b3190ce3fde4
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-02-15 15:23:11 +0700

    blog_Module

--- Server2/index.js
+++ Server2/index.js
@@ -7,6 +7,7 @@ const dbConnect = require("./config/dbConnect");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 // const cors = require("cors");
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
+app.use("/api/blog", blogRouter);
 
 app.use(notFound);
 app.use(errorHandler);
