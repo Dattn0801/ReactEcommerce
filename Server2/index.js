6f808b58a8f06c75bba70d74a31c935f0b067475
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-02-16 17:20:15 +0700

    couponUpdate

--- Server2/index.js
+++ Server2/index.js
@@ -10,6 +10,7 @@ const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
 const categoryRouter = require("./routes/categoryRoute");
 const brandRouter = require("./routes/brandRoute");
+const couponRouter = require("./routes/couponRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 // const cors = require("cors");
@@ -27,6 +28,7 @@ app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/brand", brandRouter);
+app.use("/api/coupon", couponRouter);
 app.use(notFound);
 app.use(errorHandler);
 
