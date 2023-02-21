cb0ab44d38f22b108ff58b59d0c53c8cbb786218
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-02-22 02:57:40 +0700

    helmet

--- ReactPure/client/src/pages/Ourstore.js
+++ ReactPure/client/src/pages/Ourstore.js
@@ -1,9 +1,13 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
-
+import { Helmet } from "react-helmet";
 const Ourstore = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sản phẩm</title>
+      </Helmet>
       <BreadCrumb title="Sản phẩm" />
     </>
   );
