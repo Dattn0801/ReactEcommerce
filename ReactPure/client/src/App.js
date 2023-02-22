a7e4ccc09deb43ba455e60869bc028e61685f855
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-02-22 20:30:11 +0700

    update

--- ReactPure/client/src/App.js
+++ ReactPure/client/src/App.js
@@ -6,6 +6,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import OurStore from "./pages/Ourstore";
+import Blog from "./pages/Blog";
+import CompareProduct from "./pages/CompareProduct";
+import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import Signup from "./pages/Signup";
+import SingleBlog from "./pages/SingleBlog";
+import Resetpassword from "./pages/ResetPassword";
+import RefundPloicy from "./pages/RefunPolicy";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 function App() {
   return (
     <>
@@ -16,19 +29,22 @@ function App() {
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
             <Route path="product" element={<OurStore />} />
-            {/* <Route path="product/:id" element={<SingleProduct />} />
             <Route path="blogs" element={<Blog />} />
-            <Route path="blog/:id" element={<SingleBlog />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="checkout" element={<Checkout />} />
             <Route path="compare-product" element={<CompareProduct />} />
             <Route path="wishlist" element={<Wishlist />} />
             <Route path="login" element={<Login />} />
-            <Route path="forgot-password" element={<Forgotpassword />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
             <Route path="signup" element={<Signup />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
             <Route path="reset-password" element={<Resetpassword />} />
             <Route path="privacy-policy" element={<PrivacyPolicy />} />
             <Route path="refund-policy" element={<RefundPloicy />} />
+            <Route path="product/:id" element={<SingleProduct />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
+            {/*        
+   
+              
             <Route path="shipping-policy" element={<ShippingPolicy />} />
             <Route path="term-conditions" element={<TermAndContions />} /> */}
           </Route>
