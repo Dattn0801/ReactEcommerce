3adec903fb3c4ce31bd249f3e556f34bb5d0ef51
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-08-11 15:41:03 +0700

    rate,sort

--- ReactPure/client/src/features/products/productService.js
+++ ReactPure/client/src/features/products/productService.js
@@ -2,8 +2,14 @@ import axios from "axios";
 import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/baseUrl";
 
-const getProducts = async () => {
-  const response = await axios.get(`${base_url}product`);
+const getProducts = async (data) => {
+  const response = await axios.get(
+    `${base_url}product?${data?.brand ? `brand=${data?.brand}` : ""}${data?.tags ? `tags=${data?.tags}` : ""}${
+      data?.category ? `category=${data?.category}` : ""
+    }${data?.minPrice ? `price[gte]=${data?.minPrice}` : ""}${data?.maxPrice ? `price[lte]=${data?.maxPrice}` : ""}${
+      data?.sort ? `sort=${data?.sort}` : ""
+    }`
+  );
   if (response.data) {
     return response.data;
   }
@@ -16,14 +22,14 @@ const getSingleProduct = async (id) => {
   }
 };
 
-const addToWishList = async (prodId) => {
-  const response = await axios.put(
-    `${base_url}product/wishlist`,
-    {
-      prodId,
-    },
-    config
-  );
+const addToWishList = async (data) => {
+  const response = await axios.put(`${base_url}product/wishlist`, { prodId: data.id }, data?.config2);
+  if (response.data) {
+    return response.data;
+  }
+};
+const rateProduct = async (data) => {
+  const response = await axios.put(`${base_url}product/rating`, data, config);
   if (response.data) {
     return response.data;
   }
@@ -32,4 +38,5 @@ export const productService = {
   getProducts,
   addToWishList,
   getSingleProduct,
+  rateProduct,
 };
