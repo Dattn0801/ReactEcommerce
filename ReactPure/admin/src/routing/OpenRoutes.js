631c2e12fa8374d84cf51abf3239d39c2d33ba09
Nguyen Thanh Dat <thanhdat08011999@gmail.com>
2023-08-14 17:57:26 +0700

    thêm open,private route

---
+++ ReactPure/admin/src/routing/OpenRoutes.js
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+//router khi token === undefined(login... mới đc vào)
+export const OpenRoutes = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  return user?.token === undefined ? (
+    children
+  ) : (
+    <Navigate to="/admin" replace={true} />
+  );
+};
